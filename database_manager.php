58520aa35c7363a0f265a8f9563e1b3fdd9e3f03
Alikhan Toleubay <52784852+StarLightNova@users.noreply.github.com>
2020-08-09 14:00:45 +0600

    Update database_manager.php

--- database_manager.php
+++ database_manager.php
@@ -12,9 +12,33 @@
         }
         
         else{
+            $servername   = "rudy.zzz.com.ua";
+            $login        = "starlightnova";
+            $password     = "Admin123";
+            $db           = 'starlightnova';
             
+            $conn = new mysqli($servername, $login, $password, $db);
             
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            //echo "Connected successfully";
             
+            $sql = "INSERT INTO todos (name, email, content) VALUES ( '$name', '$email', '$content')";
+            //echo $sql;
+            
+            if($conn->query($sql) == true){
+                echo "New record created successfully";
+                header("Location: http://beejeefirststage.zzz.com.ua/");
+                exit();
+            }
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "<a href = 'http://beejeefirststage.zzz.com.ua/' >back</a>";
+            }
+            
+            $conn -> close();
         }
     }
 
